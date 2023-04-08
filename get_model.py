f41d416dfe3d57fdc3b79013029dd11b75f08893
shubanms <msshuban4@gmail.com>
2023-04-08 12:18:35 +0530

    Testing script for age

--- get_model.py
+++ get_model.py
@@ -1,12 +1,28 @@
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import pickle
+import numpy as np
+import math
 
-from keras.models import model_from_json
+model = pickle.load(open('model2.pkl', 'rb'))
 
 
-def saved():
-    file = open("model.json", "r")
-    loaded = file.read()
-    file.close()
+def getAge():
 
-    model = model_from_json(loaded)
+    data1 = json["data1"]
+    input = (data1, 2, 0, 6, 6, 1, 1, 70, 0, 0,0, 11, 0, 1)  # row 1 data values
 
-    return model
+    input_data_arr = np.asarray(input)
+    print(input_data_arr)
+
+    input_data_reshaped = input_data_arr.reshape(1, -1)
+    print(input_data_reshaped)
+    prediction = model.predict(input_data_reshaped)
+    ans = math.floor(prediction)
+
+
+    return ans
+
+
+getAge()
